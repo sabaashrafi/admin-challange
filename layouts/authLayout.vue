37e80cb1f3ca7c0535081b599f2dd42b0ce2b343
s.ashrafi <fatemeaqaashrafi@gmail.com>
2020-12-20 19:51:27 +0330

    ➕ feat: add vuelidate

---
+++ layouts/authLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-container>
+      <b-col cols="11" md="8" lg="6">
+        <Nuxt />
+      </b-col>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AuthLayout'
+}
+</script>
+<style>
+
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <b-form></b-form>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'AuthLayout'
+}
+</script>
